7de6aeaa2d8a92494984888c088f15aefb04bb1a
Manvel Manucharyan <manvelmanucharyan98@gmail.com>
2022-11-20 22:27:08 +0400

    finishV2

--- routers/user.rout.js
+++ routers/user.rout.js
@@ -5,7 +5,7 @@ const auth = require("../auth/auth");
 
 router.post("/signup", UserController.signup);
 router.post("/signin", UserController.signin);
-router.get("/signin/new_token", auth.authenticateToken, UserController.updateToken);
+router.post("/signin/new_token", auth.authenticateToken, UserController.updateToken);
 router.get("/info", auth.authenticateToken, UserController.info);
 router.get("/logout", auth.authenticateToken, UserController.logout);
 
